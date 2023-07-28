814047ffa5e1b7ee54dad4941072bd0c0079fecc
Fabio Amaral <fabio.amaral@mable.com.au>
2023-07-28 21:16:37 +1000

    (graphql): adds order_type

---
+++ app/graphql/types/order_type.rb
@@ -0,0 +1,13 @@
+module 
+    class OrderType < BaseObject
+        field :id, ID, null: false
+        field :subtotal, Int, null: false
+        field :discount, Int, null: true
+        field :delivery_fee, Int, null: true
+        field :total_price, Int, null: false
+        field :payment_method_id, ID, null: false
+        field :address, String, null: true
+        field :client_id, ID, null: false
+        field :status_id, ID, null: false
+    end
+end
