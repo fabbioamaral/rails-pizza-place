5dd8f900d55110acca0f1bb54db68e343cbc322d
Fabio Amaral <fabio.amaral@mable.com.au>
2023-08-10 21:51:19 +1000

    changes query name from orders to client_orders

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -11,7 +11,8 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    field :orders, [Types::OrderType], resolver: Resolvers::OrdersResolver
+    # ORDERS
+    field :client_orders, [Types::OrderType], resolver: Resolvers::OrdersResolver
 
   end
 end
