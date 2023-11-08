558375719883fc6d9068beabc9d4061c96fc8766
Fabio Amaral <fabio.amaral@mable.com.au>
2023-11-08 20:05:18 +1100

    commit

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_product, mutation: Mutations::Products::CreateProduct
+    field :update_product, mutation: Mutations::Products::UpdateProduct
   end
 end
--- app/graphql/types/product_type.rb
+++ app/graphql/types/product_type.rb
@@ -1,13 +1,12 @@
 module Types
     class ProductType < BaseObject
-        field :id, ID, null: false
         field :name, String, null: false
         field :price, Int, null: false
         field :description, String, null: true
         field :category_id, ID, null: false
-        field :border_id, ID, null: true
+        field :pizza_border_id, ID, null: true
         field :flavour1_id, ID, null: true
         field :flavour2_id, ID, null: true
-        field :size, ID, null: true
+        field :size, String, null: false
     end
 end
