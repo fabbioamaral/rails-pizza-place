0ed724ba647342166f09b9d65301e545ac8c0194
Fabio Amaral <fabio.amaral@mable.com.au>
2023-12-04 23:18:10 +1100

    adds rack-cors

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,12 @@ module PizzaPlace
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
