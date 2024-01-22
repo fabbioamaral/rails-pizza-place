99b4ede19c5af3c775cf5aaea11ce82e6e683d90
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2024-01-22 16:52:17 +0800

    remove minitest files

--- test/models/category_flavour_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CategoryFlavourTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/suburb_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SuburbTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
