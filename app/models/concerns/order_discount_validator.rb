2e2caf4b111bfb8638882d2a40e91e56cd13f660
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2024-01-31 20:25:18 +0800

    fix error in discount validator

--- app/models/concerns/order_discount_validator.rb
+++ app/models/concerns/order_discount_validator.rb
@@ -1,5 +1,5 @@
 class OrderDiscountValidator < ActiveModel::Validator
   def validate(record)
-    record.errors.add(:status, 'discount cannot be bigger than subtotal') if record.discount > record.subtotal
+    record.errors.add(:discount, 'discount cannot be bigger than subtotal') if record.discount > record.subtotal
   end
 end
