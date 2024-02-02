367d91fa69f8ae8a2304bf3c01be0d88c9ca208d
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2024-02-02 19:06:15 +0800

    adds order discount custom validator

--- app/models/concerns/orders/order_discount_validator.rb
+++ app/models/concerns/orders/order_discount_validator.rb
@@ -1,7 +1,7 @@
 module Orders
   class OrderDiscountValidator < ActiveModel::Validator
     def validate(record)
-      record.errors.add(:discount, 'discount cannot be bigger than subtotal') if record.discount > record.subtotal
+      record.errors.add(:discount, 'discount cannot be bigger than subtotal') if (record.discount != 0 && record.discount > record.subtotal)
     end
   end
 end
