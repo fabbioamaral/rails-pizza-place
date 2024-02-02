c5a7236f18eba187c16bc991820bb55e84be1868
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2024-02-02 19:09:50 +0800

    fixing order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,16 +1,16 @@
 class Order < ApplicationRecord
+    # validations
     validates_with Orders::OrderStatusValidator
     validates_with Orders::OrderDiscountValidator
     validates_with Orders::OrderSubtotalValidator
-    
-    # validates :products , presence: true
+    validates_with Orders::OrderDeliveryFeeValidator
     validates :subtotal, numericality: { greater_than_or_equal_to: 0 }, presence: true
     validates :delivery_fee, numericality: { greater_than_or_equal_to: 0 }, presence: false # in case order type is 'in person' there is no delivery fee associated with the order
     validates :discount, numericality: { greater_than_or_equal_to: 0 }, presence: false # discounts are totally optional
     validates :total_price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, presence: true
     validates :payment_method, presence: true
     validates :order_type, presence: true
-    validates :address, presence: false # in case order type is 'in person' there is no need to record any address
+    validates :address_id, presence: false # in case order type is 'in person' there is no need to record any address
     validates :client_id, presence: true
     validates :status, presence: true
 
