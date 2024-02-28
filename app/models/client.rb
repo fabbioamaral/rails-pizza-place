8f87ec30fa7cd41b635adfdf5a64ae335e138558
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2024-02-28 16:50:15 +0700

    adds uniqueness phone number constraint to client model

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,6 @@
 class Client < ApplicationRecord
     validates :name, presence: true
-    validates :phone, presence: true
+    validates :phone, presence: true, uniqueness: true
 
     has_many :addresses
 
