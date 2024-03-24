6d5ea08b8f06779ed0e37e7bc2571f2b65cf1be8
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2024-03-24 12:12:56 +0700

    adds default column to Address table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_05_091319) do
+ActiveRecord::Schema.define(version: 2024_03_24_050839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2024_02_05_091319) do
     t.integer "client_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "default"
   end
 
   create_table "categories", force: :cascade do |t|
