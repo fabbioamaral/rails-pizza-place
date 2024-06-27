0f2e829633156fb94b9e19e0f30a9dd2fb336ced
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2024-06-27 11:50:54 -0300

    tweaks address types

--- app/graphql/types/addresses/add_address_attributes_type.rb
+++ app/graphql/types/addresses/add_address_attributes_type.rb
@@ -3,7 +3,7 @@ module Types
         class AddAddressAttributesType < Types::BaseInputObject
             argument :street, String, required: true
             argument :number, String, required: true
-            argument :additional_info, String, required: true
+            argument :additional_info, String, required: false
             argument :suburb_id, ID, required: true
             argument :client_id, ID, required: true
             argument :default, Boolean, required: true
--- app/graphql/types/addresses/update_address_attributes_type.rb
+++ app/graphql/types/addresses/update_address_attributes_type.rb
@@ -2,6 +2,12 @@ module Types
     module Addresses
         class UpdateAddressAttributesType < AddAddressAttributesType
             argument :id, ID, required: true
+            argument :street, String, required: false
+            argument :number, String, required: false
+            argument :additional_info, String, required: false
+            argument :suburb_id, ID, required: false
+            argument :client_id, ID, required: false
+            argument :default, Boolean, required: false
         end
     end
 end
