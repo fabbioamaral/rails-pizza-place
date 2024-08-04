9910c1e788ec33002566d546c052e15ccb9abe91
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2024-08-04 11:15:45 +1000

    filter pizza crusts query by pizza size

--- app/graphql/queries/crusts_query.rb
+++ app/graphql/queries/crusts_query.rb
@@ -2,9 +2,11 @@
 module Queries
     class CrustsQuery < GraphQL::Schema::Resolver
       type [Types::Pizzas::Crusts::CrustType], null: false
+
+      argument :size, String, required: true
   
-      def resolve
-        PizzaCrust.all
+      def resolve(size:)
+        PizzaCrust.where(size: size)
       end
     end
   end
