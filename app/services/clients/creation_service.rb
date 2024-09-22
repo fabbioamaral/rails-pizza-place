ebc60f864fa1a4350c46f7b673a9bafb8dbf7eaf
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2024-09-22 17:56:33 +1000

    adds client creation service

--- app/services/clients/creation_service.rb
+++ app/services/clients/creation_service.rb
@@ -5,16 +5,42 @@ module Clients
         def initialize(add_client_attributes:)
             @name = add_client_attributes[:name]
             @phone = add_client_attributes[:phone]
+            @notes = add_client_attributes[:notes]
+            @address = add_client_attributes[:address]
         end
 
         def process
-            Client.create!(
+            result = Client.create!(
                 name: @name,
                 phone: @phone,
-            )
-            { :status => true }
-            rescue StandardError
-                { :status => false }
+                notes: @notes
+                )
+                @client_id = result.id
+                
+                create_address
+                { :status => true }
+            rescue StandardError => e
+                { :status => false, :error => "Error message: #{e.message}" }
+            end
+            
+            private 
+            
+            def address_attributes
+                OpenStruct.new({
+                    street: @address[:street],
+                    number: @address[:number],
+                    suburb_id: @address[:suburb_id],
+                    city: @address[:city],
+                    client_id: @client_id,
+                    default: true
+                })
+            end
+            
+            def create_address
+                # binding.pry 
+            ::Addresses::CreationService.new(
+                add_address_attributes: address_attributes
+            ).process
         end
     end
 end
